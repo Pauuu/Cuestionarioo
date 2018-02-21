82d787179bfc725f1f76286eeb53493c6144ea98
Pau <belcegorx@gmail.com>
2018-02-21 10:40:20 +0100

    Actualizacion

--- js/js.js
+++ js/js.js
@@ -18,75 +18,52 @@ window.onload = function () {
 
 // función personalizada que gestiona la respuesta a la petición de fichero
 function gestionarJson(dadesJson) {
-  var obj = JSON.parse(dadesJson);
 
+  var obj = JSON.parse(dadesJson);
 
   var questionLength = Object.keys(obj.question).length;
 
 
+  //TEXT****************************/
+  var preguntasText = [];
 
-  for (q = 0; q < obj.question.length; q++) {
-
-
-    document.getElementsByTagName("h3")[q].innerHTML = obj.question[q].title;
-
-
-    //SEELECT
-    if (obj.question[q].type == "select") {
-
-      var opcionesSelect = [];
-      var nopt = obj.question[q].option.length;
-
-      for (i = 0; i < nopt; i++) {
-        opcionesSelect[i] = obj.question[q].option[i];
-      }
-      mostrarSelect(q, opcionesSelect);
-    }
-	
-	//CHECKBOX
-    if (obj.question[q].type == "checkbox") {
-
-      var opcionesCheckbox = [];
-      var nopt = obj.question[q].option.length;
-
-      for (i = 0; i < nopt; i++) {
-        opcionesCheckbox[q] = obj.question[q].option[i];
-      }
-    }
+  for (i = 0; i < 2; i++) { //  TODOS LOS BUCLES EMPIEZAN POR 0 HASTA LLEGRAR A LA PREGUNTA i-1
+    preguntasText[i] = obj.question[i].title;
   }
+  mostrarText(preguntasText);
+
+  //**text**************************/
+
+  //SELECT**************************/
+  var preguntaSelect;
+  var opSelect = [];
+
+  for (i = 2; i < 4; i++) {  //desde pregunta 3 a 4 (i-1)
+    preguntaSelect = obj.question[i].title;
+
+    for (preg = 0; preg < obj.question[i].option.length; preg++) {
+      opSelect[preg] = obj.question[i].option[preg];
+    }
+    mostrarSelect(i, preguntaSelect, opSelect);
+  }
+  //**select************************/
 }
 
 //FUNCIONES
 
-
-function presguntasNumber(numQuestion) {
-  numeroSecreto = obj.question[numQuestion];
-}
-
-function mostrarCheckbox(q, opcionCheckbox) {
-
-  for (i = 0; i < opcionCheckbox.length; i++) {
-
-    var input = document.createElement("input");
-    var label = document.createElement("label");
-    label.innerHTML = opcionesCheckbox[i];
-    input.
+function mostrarText(pregunta) {
+  for (i = 0; i < pregunta.length; i++) {
+    document.getElementsByTagName("h3")[i].innerHTML = pregunta[i];
   }
 }
 
-function mostrarSelect(q, opcionSelect) {
 
-  for (i = 0; i < opcionSelect.length; i++) {
-    document.getElementsByTagName("select")[q].getElementsByTagName("option")[i].innerHTML = opcionSelect[i];
-  }
-}
+function mostrarSelect(numPreg, pregunta, opciones) {
+  var createOption = document.createElement('option');
 
-function presguntasNumber(numQuestion) {
-  numeroSecreto = obj.question[numQuestion];
-}
+  document.getElementsByTagName("h3")[numPreg].innerHTML = pregunta;
 
-function titulos() { //cambiar
-  for (i = 0; i < questionLength; i++) {
-    document.getElementsByTagName("h3")[i].innerHTML = obj.question[i].title;
-  }
+  document.getElementsByTagName("select")[0].createOption;
+
+  document.getElementsByName("select")[0].getElementsByTagName("option")[0].innerHTML = pregunta;
 }
